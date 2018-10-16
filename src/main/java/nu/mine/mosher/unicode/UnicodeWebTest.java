9f9656a109d6ae3496c2d703988183583c61ca2d
Christopher Mosher <cmosher@coreinformatics.com>
2018-10-16 15:38:26 -0400

    add compact display and configurable row length

--- src/main/java/nu/mine/mosher/unicode/UnicodeWebTest.java
+++ src/main/java/nu/mine/mosher/unicode/UnicodeWebTest.java
@@ -1,20 +1,21 @@
 package nu.mine.mosher.unicode;
 
+import com.google.common.math.LongMath;
+import com.google.common.primitives.Longs;
 import fi.iki.elonen.NanoHTTPD;
 import org.stringtemplate.v4.STGroupFile;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import static fi.iki.elonen.NanoHTTPD.SOCKET_READ_TIMEOUT;
 import static java.lang.Runtime.getRuntime;
+import static java.math.RoundingMode.CEILING;
 import static java.util.Collections.emptyList;
 
 public class UnicodeWebTest {
-    private static final int ROW_LENGTH = 0x20;
-    private static final int ROW_COUNT = 0x80;
+    private static final int PAGE_SIZE = 0x1000;
 
     public static void main(final String... args) throws IOException {
         final UnicodeManager unicodeMgr = UnicodeManager.create();
@@ -25,15 +26,12 @@ public class UnicodeWebTest {
             @Override
             public Response serve(final IHTTPSession session) {
                 try {
-                    final List<String> paramStart = session.getParameters().getOrDefault("start", emptyList());
-                    long start = 0;
-                    if (!paramStart.isEmpty()) {
-                        try {
-                            start = Long.parseLong(paramStart.get(0), 0x10);
-                        } catch (final Throwable ignore) {
-                        }
-                    }
-                    return newFixedLengthResponse(Response.Status.OK, MIME_HTML, fixedPage(stg, unicodeMgr, start, new Pager(start, unicodeMgr.getMaxCodepoint())));
+                    final long start = longParam(session, "start", 0L);
+                    final boolean compact = booleanParam(session, "compact", false);
+                    final int rowlen = (int)1L << Longs.constrainToRange(longParam(session, "row", 0x5L), 0L, LongMath.log2(PAGE_SIZE, CEILING));
+                    return newFixedLengthResponse(
+                            Response.Status.OK, MIME_HTML,
+                            fixedPage(stg, unicodeMgr, start, new Pager(start, unicodeMgr.getMaxCodepoint(), compact, rowlen)));
                 } catch (final Throwable e) {
                     throw new IllegalStateException(e);
                 }
@@ -47,26 +45,58 @@ public class UnicodeWebTest {
         System.err.flush();
     }
 
+    private static boolean booleanParam(final NanoHTTPD.IHTTPSession session, final String param, boolean def) {
+        boolean value = def;
+        final List<String> values = session.getParameters().getOrDefault(param, emptyList());
+        if (!values.isEmpty()) {
+            try {
+                value = Boolean.parseBoolean(values.get(0));
+            } catch (final Throwable ignore) {
+            }
+        }
+        return value;
+    }
+
+    private static long longParam(final NanoHTTPD.IHTTPSession session, final String param, long def) {
+        long value = def;
+        final List<String> values = session.getParameters().getOrDefault(param, emptyList());
+        if (!values.isEmpty()) {
+            try {
+                value = Long.parseLong(values.get(0), 0x10);
+            } catch (final Throwable ignore) {
+            }
+        }
+        return value;
+    }
+
     public static class Pager {
         public final long start;
         public final long prev;
         public final long next;
         public final long maxCodepoint;
+        public final boolean compact;
+        public final int rowlen;
 
-        Pager(long start, long maxCodepoint) {
+        Pager(long start, long maxCodepoint, boolean compact, int rowlen) {
             this.start = start;
-            this.prev = start - ROW_COUNT * ROW_LENGTH;
-            this.next = start + ROW_COUNT * ROW_LENGTH;
+            this.prev = start - PAGE_SIZE;
+            this.next = start + PAGE_SIZE;
             this.maxCodepoint = maxCodepoint;
+            this.compact = compact;
+            this.rowlen = rowlen;
         }
 
         public String getStart() {
-            return String.format("U+%03X\u00d7\u00d7\u00d7", this.start/0x1000);
+            return String.format("U+%03X\u00d7\u00d7\u00d7", this.start/PAGE_SIZE);
+        }
+
+        public int getRowlen() {
+            return this.rowlen;
         }
 
         public String getPrev() {
             if (this.prev < 0) {
-                return f(this.maxCodepoint / 0x1000 * 0x1000);
+                return f(this.maxCodepoint / PAGE_SIZE * PAGE_SIZE);
             }
             return f(this.prev);
         }
@@ -78,23 +108,28 @@ public class UnicodeWebTest {
             return f(this.next);
         }
 
+        public boolean isCompact() {
+            return this.compact;
+        }
+
         private String f(long cp) {
             return String.format("%06X", cp);
         }
     }
 
     private static String fixedPage(final STGroupFile stg, final UnicodeManager unicodeMgr, final long start, final Pager pager) {
+        final int rlen = pager.rowlen;
         long cp = start;
-        final List<List<UnicodeCharacter>> ctab = new ArrayList<>(ROW_COUNT);
-        for (int r = 0; r < ROW_COUNT; ++r) {
-            final List<UnicodeCharacter> cols = new ArrayList<>(ROW_LENGTH);
+        final List<List<UnicodeCharacter>> ctab = new ArrayList<>(PAGE_SIZE / rlen);
+        for (int r = 0; r < PAGE_SIZE / rlen; ++r) {
+            final List<UnicodeCharacter> cols = new ArrayList<>(rlen);
             ctab.add(cols);
-            for (int c = 0; c < ROW_LENGTH; ++c) {
+            for (int c = 0; c < rlen; ++c) {
                 cols.add(unicodeMgr.charFor(cp++));
             }
         }
         return stg
-                .getInstanceOf("fixedPage")
+                .getInstanceOf(pager.compact ? "compactPage" : "fixedPage")
                 .add("ctab", ctab)
                 .add("pager", pager)
                 .render();
